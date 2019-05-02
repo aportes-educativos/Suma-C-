a48b313a6ec89d605cebcb0d4c5773d2868b5348
Jhonny27 <50205548+Jhonny27@users.noreply.github.com>
2019-05-02 08:35:28 -0500

    Create Sumario.cpp

---
+++ Sumario.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int num1, num2, resultado;
+cout<<"Digita el primer número a sumar: ";
+cin>>num1;
+cout<<" Digita el segundo número a sumar: ";
+cin>>num2;
+resultado = num1+num2;
+cout<<" La suma es: "<<resultado<<endl;
+}
